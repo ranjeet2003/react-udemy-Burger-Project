c906987b518198a0b8b5ee7489aabb94079e04d6
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-05 19:37:35 +0530

    using state first time

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    person: [
+      { name: 'Ranjeet', age: 20 },
+      { name: 'Pushpendra', age: 23 },
+      { name: 'Baba', age: 72 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello I am Ranjeet Gautam</h1>
         <h1>Hello</h1>
         <p>I am a student</p>
-        <Person name="Ranjeet" age="20"></Person>
-        <Person name="Pushpendra" age="23">Pushpa is preparing for gate 2020 </Person>
-        <Person name="BABA" age="72"></Person>
+        <button>Switch Name</button>
+        <Person name={ this.state.person[0].name } age={ this.state.person[0].age }></Person>
+        <Person name={ this.state.person[1].name } age={ this.state.person[1].age }>Pushpa is preparing for gate 2020 </Person>
+        <Person name={ this.state.person[2].name } age={ this.state.person[2].age }></Person>
       </div>
     );
   }
