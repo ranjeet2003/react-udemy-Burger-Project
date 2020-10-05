59b59af01a3482c49f5b89d430ba33419d83c9a6
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-05 21:37:28 +0530

    passing method reference between two components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <h1>Hi, My name is {props.name} and I am {props.age} years old.</h1>
+            <h1 onClick = { props.click } > Hi, My name is {props.name} and I am {props.age} years old.</h1>
             <h3>{props.children}</h3>
         </div>
     )
