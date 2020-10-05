44a28765768a6b5d75bb8ee16faf35aedfb172be
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-05 18:50:28 +0530

    passing info using props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const person = (props) => {
-return <h1>Hi, My name is{ props.name } and I am { props.age } years old.</h1>
+return <h1>Hi, My name is { props.name } and I am { props.age } years old.</h1>
 };
  export default person;
