9aa03a5ea3080658dc07cda9b48cab5a6f4e0d81
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-05 22:19:07 +0530

    working with style

--- src/App.js
+++ src/App.js
@@ -35,11 +35,20 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColour: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hello I am Ranjeet Gautam</h1>
         <p>I am a student</p>
-        <button onClick={ () => this.switchNameHandler('Ranjeet (passing parameter) !')}>Switch Name</button>
+        <button style = {style} onClick={ () => this.switchNameHandler('Ranjeet (passing parameter) !')}>Switch Name</button>
         <Person
           name={this.state.person[0].name}
           age={this.state.person[0].age}>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className = "Person">
             <h1 onClick = { props.click } > Hi, My name is {props.name} and I am {props.age} years old.</h1>
             <h3>{props.children}</h3>
             <input type = "text" onChange = {props.changed} value = {props.name}/>
