2ba50281dcea41d2d5e4da3ee4df6622d2f4d57c
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-05 20:42:20 +0530

    updating state using setState

--- src/App.js
+++ src/App.js
@@ -12,20 +12,27 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () =>{
-    console.log('Button is clicked and switchNameHandler is called !');
+  switchNameHandler = () => {
+    // console.log('Button is clicked and switchNameHandler is called !');
+    // this.state.person[0].name = 'Ranjeet Gautam'  this wouldn't work if we want to update state.
+    this.setState({
+      person: [
+        { name: 'Ranjeet Gautam', age: 20 },
+        { name: 'Pushpendra', age: 23 },
+        { name: 'Baba', age: 72 }
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hello I am Ranjeet Gautam</h1>
-        <h1>Hello</h1>
         <p>I am a student</p>
-        <button onClick = { this.switchNameHandler }>Switch Name</button>
-        <Person name={ this.state.person[0].name } age={ this.state.person[0].age }></Person>
-        <Person name={ this.state.person[1].name } age={ this.state.person[1].age }>Pushpa is preparing for gate 2020 </Person>
-        <Person name={ this.state.person[2].name } age={ this.state.person[2].age }></Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>Pushpa is preparing for gate 2020 </Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}></Person>
       </div>
     );
   }
